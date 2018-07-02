95d3b1cfc34d5cefa65387fa31db8c421a7ff8a5
David Mills <dave@1morepx.com>
2018-07-02 16:09:34 -0400

    refactor convertMonth to a function

--- talkingCalendar.js
+++ talkingCalendar.js
@@ -3,6 +3,37 @@
 // November 11th, 2007
 // August 24th, 1987
 
+var convertMonth = function(month) {
+  switch (month) {
+    case 1:
+      return "January";
+    case 2:
+      return "February";
+    case 3:
+      return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+    case 6:
+      return "June";
+    case 7:
+      return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+    default:
+      return "";
+  }
+}
+
 var talkingCalendar = function(date) {
   // Your code here
   var components = date.split("/");
@@ -11,48 +42,7 @@ var talkingCalendar = function(date) {
   var month = parseInt(components[1], 10);
   var day = parseInt(components[2], 10);
 
-  var monthString;
-
-  switch (month) {
-    case 1:
-      monthString = "January";
-      break;
-    case 2:
-      monthString = "February";
-      break;
-    case 3:
-      monthString = "March";
-      break;
-    case 4:
-      monthString = "April";
-      break;
-    case 5:
-      monthString = "May";
-      break;
-    case 6:
-      monthString = "June";
-      break;
-    case 7:
-      monthString = "July";
-      break;
-    case 8:
-      monthString = "August";
-      break;
-    case 9:
-      monthString = "September";
-      break;
-    case 10:
-      monthString = "October";
-      break;
-    case 11:
-      monthString = "November";
-      break;
-    case 12:
-      monthString = "December";
-      break;
-    default:
-      break;
-  }
+  var monthString = convertMonth(month);
 
   return monthString + " " + day + ", " + year;
 };
