35926dae9dba15b31241d6187a7156449c2c7647
David Mills <dave@1morepx.com>
2018-07-02 15:55:20 -0400

    splitting date and returning in the correct order

--- talkingCalendar.js
+++ talkingCalendar.js
@@ -1,7 +1,19 @@
+// Expected Output:
+// December 2nd, 2017
+// November 11th, 2007
+// August 24th, 1987
+
 var talkingCalendar = function(date) {
   // Your code here
+  var components = date.split('/');
+
+  var year = components[0];
+  var month = components[1];
+  var day = components[2];
+
+  return month + " " + day + ", " + year;
 };
 
 console.log(talkingCalendar("2017/12/02"));
-console.log(talkingCalendar("2007/11/11"));
-console.log(talkingCalendar("1987/08/24"));
+// console.log(talkingCalendar("2007/11/11"));
+// console.log(talkingCalendar("1987/08/24"));
